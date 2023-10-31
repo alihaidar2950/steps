8b215c4b9849ed2abdc0070fbab0128c046c6745
alihaidar2950 <alihaidar2950@gmail.com>
2023-10-31 19:48:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,18 +13,20 @@ export default function App() {
   // const [test, setTest] = useState({ name: 'Ali' });
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
 
     // setTest({ name: 'Fred' });
   }
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
       {isOpen && (
